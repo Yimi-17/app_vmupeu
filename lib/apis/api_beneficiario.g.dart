ee272b8c45527b76514e34c7a86a1bfe43b3ab5d
Yimi-17 <54036463+Yimi-17@users.noreply.github.com>
2022-06-20 23:14:32 -0500

    Initial commit APP

---
+++ lib/apis/api_beneficiario.g.dart
@@ -0,0 +1,148 @@
+part of 'api_beneficiario.dart';
+
+class _BeneficiarioApi implements BeneficiarioApi {
+  _BeneficiarioApi(this._dio, {this.baseUrl}) {
+    ArgumentError.checkNotNull(_dio, '_dio');
+    this.baseUrl ??= "http://127.0.0.1:5000";
+  }
+
+  final Dio _dio;
+  String baseUrl;
+
+  @override
+  getBeneficiario() async {
+    final prefs = await SharedPreferences.getInstance();
+    var tokenx = prefs.getString("token");
+    print("VERListar: ${tokenx}");
+    //ArgumentError.checkNotNull(tokenx, "token");
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<List<dynamic>> _result =
+        await _dio.request('/api/beneficiario',
+            queryParameters: queryParameters,
+            options: RequestOptions(
+                method: 'GET',
+                headers: <String, dynamic>{
+                  /*"Authorization":tokenx*/
+                },
+                extra: _extra,
+                baseUrl: baseUrl),
+            data: _data);
+    var value = _result.data
+        .map((dynamic i) =>
+            BeneficiarioModel.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return Future.value(value);
+  }
+
+  @override
+  getBeneficiarioId(token, id) async {
+    ArgumentError.checkNotNull(id, 'id');
+    ArgumentError.checkNotNull(token, 'token');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<List<dynamic>> _result = await _dio.request(
+        '/api/beneficiario/$id',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'GET',
+            headers: <String, dynamic>{"Authorization": token},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    //final value = ModeloPersona.fromJson(_result.data);
+    //return Future.value(value);
+    var value = _result.data
+        .map((dynamic i) =>
+            BeneficiarioModel.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return Future.value(value);
+  }
+
+  @override
+  login(user) async {
+    ArgumentError.checkNotNull(user, "user");
+    print("ver: ${user.toJson()}");
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(user.toJson() ?? <String, dynamic>{});
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        '/api/auth',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'POST',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    var value = TokenModel.fromJson(_result.data);
+    return Future.value(value);
+  }
+
+  @override
+  deleteBeneficiario(id) async {
+    final prefs = await SharedPreferences.getInstance();
+    var tokenx = prefs.getString("token");
+    ArgumentError.checkNotNull(id, '0');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        '/api/beneficiario/$id',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'DELETE',
+            headers: <String, dynamic>{"Authorization": tokenx},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = MsgModel.fromJson(_result.data);
+    return Future.value(value);
+  }
+
+  @override
+  updateBeneficiario(id, beneficiario) async {
+    ArgumentError.checkNotNull(id, '0');
+    ArgumentError.checkNotNull(beneficiario, 'beneficiario');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(beneficiario.toJson() ?? <String, dynamic>{});
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        '/api/beneficiariout/$id',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'PATCH',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = MsgModel.fromJson(_result.data);
+    return Future.value(value);
+  }
+
+  @override
+  createBeneficiario(beneficiario) async {
+    ArgumentError.checkNotNull(beneficiario, 'beneficiario');
+    final prefs = await SharedPreferences.getInstance();
+    var tokenx = prefs.getString("token");
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(beneficiario.toJson() ?? <String, dynamic>{});
+    final Response<Map<String, dynamic>> _result = await _dio.request(
+        '/api/beneficiario/crear',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'POST',
+            headers: <String, dynamic>{"Authorization": tokenx},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = MsgModel.fromJson(_result.data);
+    return Future.value(value);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:app_vmupeu/drawer/navigation_home_screen.dart';
+import 'package:app_vmupeu/login/login_google.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Map<int, Color> color = {
+    50: const Color.fromRGBO(250, 202, 88, .1),
+    100: const Color.fromRGBO(250, 202, 88, .2),
+    200: const Color.fromRGBO(250, 202, 88, .3),
+    300: const Color.fromRGBO(250, 202, 88, .4),
+    400: const Color.fromRGBO(250, 202, 88, .5),
+    500: const Color.fromRGBO(250, 202, 88, .6),
+    600: const Color.fromRGBO(250, 202, 88, .7),
+    700: const Color.fromRGBO(250, 202, 88, .8),
+    800: const Color.fromRGBO(250, 202, 88, .9),
+    900: const Color.fromRGBO(250, 202, 88, 1),
+  };
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(), //Resultado al iniciar sesion
+    );
+  }
+}
